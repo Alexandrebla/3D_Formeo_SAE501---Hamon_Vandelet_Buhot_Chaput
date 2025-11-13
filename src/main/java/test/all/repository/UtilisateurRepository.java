f75081f452604918a6447eb9f034c8f31c4ca19b
Yanisbht <165911737+Yanisbht@users.noreply.github.com>
2025-11-13 12:20:18 +0100

    Add files via upload

---
+++ src/main/java/test/all/repository/UtilisateurRepository.java
@@ -0,0 +1,9 @@
+package test.all.repository;
+
+import test.all.model.Utilisateur;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UtilisateurRepository extends JpaRepository<Utilisateur, Long> {
+}
