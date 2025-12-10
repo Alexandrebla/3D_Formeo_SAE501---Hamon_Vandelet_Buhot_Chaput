7fb880aaa2534a4633b694e4c183d2606292693b
TheooChaput <chaputtheo@gmail.com>
2025-12-10 11:46:26 +0100

    Add files via upload
    Intervenant Repository

---
+++ demo/demo/src/main/java/com/example/demo/Repository/IntervenantRepository.java
@@ -0,0 +1,9 @@
+package com.example.demo.Repository;
+
+import com.example.demo.Entity.Intervenant;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface IntervenantRepository extends JpaRepository<Intervenant, Long> {
+
+}
+
