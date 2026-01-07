d6c7b3ff1e5f92635fe1f23e1c9f6bd5deeb8a76
Alexandrebla <165911663+Alexandrebla@users.noreply.github.com>
2026-01-07 21:46:45 +0100

    Delete demo directory

--- demo/demo/src/main/java/com/example/demo/Repository/IntervenantRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.Repository;
-
-import com.example.demo.Entity.Intervenant;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface IntervenantRepository extends JpaRepository<Intervenant, Long> {
-
-}
-
